0e49de90b7fa304649d45cae55f21d87837fe1ce
Vladimir A. Goshev (sunx.me) <sunx_git@sunx.name>
2016-12-12 20:23:11 +0100

    Add config file support

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
-
 #Simple static site generator
-# $1 - file name
 
 cd `dirname $0`/../
+
+############
+## Config ##
+############
+#Directory where script will store compiled files
+CACHE_DIR="./cache"
+#Directory, where original files are stored
+DATA_DIR="./data"
+
+#Different pages
+PAGE_HEADER="./data/default/header.htm"
+PAGE_FOOTER="./data/default/footer.htm"
+PAGE_404="./data/default/404"
+PAGE_INDEX="./data/default/index"
+
+[ -f ./conf/conf ] && . ./conf/conf
+
 
 FILE=`basename "$REQUEST_URI"`
 
